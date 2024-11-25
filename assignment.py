b992aea2ae2f3fd559f07cd5ef51ccd3f8533bc9
Harsh Bhansali <hbhansali007@gmail.com>
2024-11-25 19:45:32 +0530

    add inital setup

---
+++ assignment.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, firstName, lastName, birthYear) -> None:
+        pass
+        
+class Circle:
+    def __init__(self, radius) -> None:
+        pass
+
+class Vehicle:
+    pass
+
+class ElectricVehicle:
+    pass
+
+class DynamicClass:
+    pass
+
+class ValidatedAttribute:
+    pass
